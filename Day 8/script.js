bdf6edc42dd5711bbbc896ec21b4a8cb43741401
Prachi <prachiaky53@gmail.com>
2026-02-02 15:58:46 +0530

    commit

---
+++ Day 8/script.js
@@ -0,0 +1,33 @@
+// DOM It represents the structure and content of a web page allowing developers to interact with,manipulate and update the content of a webpage
+//  ID TRAVERSAL
+const heading= document.getElementById("myheading");
+ console.log(heading);
+
+ heading.textContent="Hello DOM!";
+
+
+ heading.style.color="tomato";
+ heading.style.backgroundColor="yellow";
+
+// CLASS TRAVERSAL
+
+const para= document.getElementsByClassName("para");
+console.log(para);
+for(let i=0; i<para.length; i++ ){
+  para[i].style.backgroundColor="cyan";
+}
+
+<p id="contentp"> This is content paragraph </p>    
+// queryselector
+const qs=document.querySelector("#contentp")
+console.log(qs);
+qs.textContent= "vfngjnvkjdkdj";
+
+
+
+
+
+
+
+
+
